5851c53a2303f7bf23e3575b7374763de5213801
Duru Harshitha <22ee01071@iitbbs.ac.in>
2024-06-04 18:01:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,53 +1,19 @@
-// ******************************* 
-// START HERE IF YOU WANT AN EASIER STARTING POINT FOR THIS ASSIGNMENT
-// *******************************
-//
-// Module 4 Assignment Instructions.
-//
-// The idea of this assignment is to take an existing array of names
-// and then output either Hello 'Name' or Good Bye 'Name' to the console.
-// The program should say "Hello" to any name except names that start with a "J"
-// or "j", otherwise, the program should say "Good Bye". So, the final output
-// on the console should look like this:
-/*
-Hello Yaakov
-Good Bye John
-Good Bye Jen
-Good Bye Jason
-Hello Paul
-Hello Frank
-Hello Larry
-Hello Paula
-Hello Laura
-Good Bye Jim
+(function() {
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-WARNING!!! WARNING!!!
-The code does NOT currently work! It is YOUR job to make it work
-as described in the requirements and the steps in order to complete this
-assignment.
-WARNING!!! WARNING!!!
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
 
-*/
+    var greeting;
+    if (firstLetter === 'j') {
+      greeting = byeSpeaker.speak(names[i]);
+    } else {
+      greeting = helloSpeaker.speak(names[i]);
+    }
 
-// STEP 1: (NOTHING TO DO. ALREADY DONE FOR YOU)
-// Wrap the entire contents of script.js inside of an IIFE
-// See Lecture 52, part 2
-// (Note, Step 2 will be done in the SpeakHello.js file.)
-(function () {
-
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
-
-for (var i = 0; i < names.length; i++) {
-  var firstLetter = names[i].charAt(0).toLowerCase();
-
-  var greeting;
-            if (firstLetter === 'j') {
-                greeting = byeSpeaker.speak(names[i]);
-            } else {
-                greeting = helloSpeaker.speak(names[i]);
-            }
-  
-            document.getElementById("greeting").textContent = greeting;
-}
-
+    var greetingElement = document.getElementById("greeting");
+    var newGreeting = document.createElement("div");
+    newGreeting.textContent = greeting;
+    greetingElement.appendChild(newGreeting);
+  }
 })();
